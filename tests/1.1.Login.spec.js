ebb9b484700e19edb2b53f06a7bd8017f52f0e99
playwright-project <andriitest7799@gmail.com>
2024-12-21 00:16:24 +0200

    +

--- tests/1.1.Login.spec.js
+++ tests/1.1.Login.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 const { URLs } = require('../Common/Urls');
 
 test('Login with valid credentials', async ({ page }) => {
-// Navigate to the Testing101 website
+  // Navigate to the Testing101 website
 await page.goto(URLs.pageLinkHomePage);
 await page.waitForTimeout(5000);
 //Click on the Consent button on Cookie pop-up
@@ -44,7 +44,7 @@ await expect(page.getByText('Make sure you enter a password.')).toBeVisible();
 
 test('Login with an empty Email field of the Login form', async ({ page }) => {
 // Navigate to the Testing101 website
- await page.goto(URLs.pageLinkHomePage);
+await page.goto(URLs.pageLinkHomePage);
 await page.waitForTimeout(5000);
 //Click on the Consent button on Cookie pop-up
 await page.getByLabel('Consent', { exact: true }).click();
